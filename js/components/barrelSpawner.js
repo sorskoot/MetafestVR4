4715abb888e91acfd04bdcc68fcbb5f9710fcf6b
Timmy Kokke <mental_c@hotmail.com>
2021-03-05 23:46:11 +0100

    made barrel-spawner use setInterval

--- js/components/barrelSpawner.js
+++ js/components/barrelSpawner.js
@@ -7,14 +7,10 @@ WL.registerComponent('barrel-spawner', {
     init: function() {
         
     },
-    start: function() {
-        this.counter = 0;
+    start: function() {      
+        setInterval(this.spawn.bind(this),this.spawnrate*1000);
     },
-    update: function(dt) {
-        this.counter += dt;
-
-        if(this.counter > this.spawnrate){
-            this.counter=0;
+    spawn: function() {      
             let barrel = WL.scene.addObject(this.barrelsLayer);
             let barrelMesh = WL.scene.addObject(barrel);
             let mesh = barrelMesh.addComponent('mesh');
@@ -23,13 +19,12 @@ WL.registerComponent('barrel-spawner', {
             barrelMesh.translate([0,-0.175,0]);
 
             barrel.translate([Math.random()*6-3,0,Math.random()*6-3]);
-            barrel.addComponent('float');
+            barrel.addComponent('float',{depth:.1});
             barrel.addComponent('tags',{tags:'barrel'});
             let collisionComponent = barrel.addComponent('collision');
             collisionComponent.collider = WL.Collider.Box;
             collisionComponent.group = 1 << 3;
             collisionComponent.extents = [0.11,0.17,.11];
             collisionComponent.active = true;
-        }
     },
 });
