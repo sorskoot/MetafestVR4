864b1271f9b2ed4a3adc5ac3bc12c3ccd1d7e7da
Timmy Kokke <mental_c@hotmail.com>
2021-03-17 23:14:45 +0100

    tiny improvements and fixes

---
+++ js/components/ice-drift.js
@@ -0,0 +1,42 @@
+WL.registerComponent('ice-drift', {
+    startLocation: { type: WL.Type.Object },
+    endLocation: { type: WL.Type.Object },
+    speed: { type: WL.Type.Float, default: 5.0 },
+    isDrifting: { type: WL.Type.Bool, default: false },
+    iceController: {type: WL.Type.Object, default: null}
+}, {
+    init: function () {                
+        this.currentPosition = new Float32Array(3);
+        this.fromPosition = [];
+        this.startLocation.getTranslationWorld(this.fromPosition);
+
+        this.toPosition = [];
+        this.endLocation.getTranslationWorld(this.toPosition);
+
+        glMatrix.vec3.sub(this.currentPosition, this.toPosition, this.fromPosition);
+        this.fromToLength = glMatrix.vec3.length(this.currentPosition);
+        this.currentLength = 0;
+
+        this.object.setTranslationWorld(this.currentPosition);
+    },
+
+    update: function (dt) {
+        if (this.isDrifting) {
+            this.currentLength += dt * this.speed;
+            let factor = this.currentLength / this.fromToLength;
+            factor = Math.min(1.0, factor);
+            glMatrix.vec3.lerp(this.currentPosition, this.fromPosition, this.toPosition, factor);
+            let position = [];
+            this.object.getTranslationWorld(position);
+            //this.object.resetTranslationRotation();
+            this.object.setTranslationWorld(
+                [this.currentPosition[0],position[1],this.currentPosition[2]]);
+
+            if (this.currentLength >= this.fromToLength) {
+                //game.iceAtDestination(this.iceController.getComponent('ice-controller'));
+                this.isDrifting = false;                
+            }
+           
+        }
+    },
+});
